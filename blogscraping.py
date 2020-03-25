e77ab4c937beb20d822f89cc860d35b4d2839956
segwuonwu <segwuonwu@gmail.com>
2020-03-25 01:40:12 -0700

    Readme

---
+++ blogscraping.py
@@ -0,0 +1,22 @@
+import requests
+from bs4 import BeautifulSoup
+from csv import writer
+
+response = requests.get('https://webscraper.io/test-sites/e-commerce/allinone')
+
+soup = BeautifulSoup(response.text, 'html.parser')
+
+posts = soup.find_all(class_='col-sm-4')
+
+with open('post.csv', 'w') as csv_files:
+    csv_writer = writer(csv_files)
+    headers = ['Title', 'Links', 'Price']
+    csv_writer.writerow(headers)
+
+    for post in posts: 
+        title = post.find(class_='title').get_text().replace('\n', '')
+        link = post.find('a')['href']
+        price = post.select('.price')[0].get_text()
+        csv_writer.writerow([title, link, price])
+       
+
